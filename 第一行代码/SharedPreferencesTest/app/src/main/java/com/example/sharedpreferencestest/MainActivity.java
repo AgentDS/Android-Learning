07ca7e0f61540f4e87b0eadadf90e31505f63039
AgentDS <zszxlsq@gmail.com>
2020-12-07 16:45:07 +0800

    ..

--- 第一行代码/SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
+++ 第一行代码/SharedPreferencesTest/app/src/main/java/com/example/sharedpreferencestest/MainActivity.java
@@ -2,15 +2,17 @@ package com.example.sharedpreferencestest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
-    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +22,13 @@ public class MainActivity extends AppCompatActivity {
         saveData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_MULTI_PROCESS).edit();
+                SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
                 editor.putString("name", "Tom");
                 editor.putInt("age", 28);
                 editor.putBoolean("married", false);
                 editor.apply();
             }
         });
-
         Button restoreData = (Button) findViewById(R.id.restore_data);
         restoreData.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,10 +37,11 @@ public class MainActivity extends AppCompatActivity {
                 String name = pref.getString("name", "");
                 int age = pref.getInt("age", 0);
                 boolean married = pref.getBoolean("married", false);
-                Log.d(TAG, "name is " + name);
-                Log.d(TAG, "age is " + age);
-                Log.d(TAG, "married is " + married);
+                Log.d("MainActivity", "name is " + name);
+                Log.d("MainActivity", "age is " + age);
+                Log.d("MainActivity", "married is " + married);
             }
         });
     }
+
 }
